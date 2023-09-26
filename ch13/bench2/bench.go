7fb3235999f9bfe5a8e43264a89aebe0bd99678b
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-09-26 15:07:56 +0900

    benchマークのテストサンプルを追加

---
+++ ch13/bench2/bench.go
@@ -0,0 +1,21 @@
+package bench2
+
+import "os"
+
+func FileLen(f string, bufsize int) (int, error) { //liststart
+	file, err := os.Open(f)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+	count := 0
+	for {
+		buf := make([]byte, bufsize)
+		num, err := file.Read(buf)
+		count += num
+		if err != nil {
+			break
+		}
+	}
+	return count, nil
+} //listend
