bcf125cf8479a7ca94082384254ae0aec7ace05c
iruma-tea <shusuke.think.pad@gmail.com>
2023-09-30 20:28:45 +0900

    14.1.8まで学習完了

---
+++ ch14/ex1409/ex1409.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"time"
+)
+
+func MakeTimedFunction(f any) any {
+	rf := reflect.TypeOf(f)
+	if rf.Kind() != reflect.Func {
+		panic("関数を指定してください")
+	}
+	vf := reflect.ValueOf(f)
+	wrapperF := reflect.MakeFunc(rf, func(in []reflect.Value) (results []reflect.Value) {
+		start := time.Now()
+		out := vf.Call(in)
+		end := time.Now()
+		fmt.Printf("%sの所要時間: %v\n", runtime.FuncForPC(vf.Pointer()).Name(),
+			end.Sub(start))
+		return out
+	})
+	return wrapperF.Interface()
+}
+
+func timeMe() { //liststart2
+	time.Sleep(1 * time.Second)
+}
+
+func timeMeToo(a int) int {
+	time.Sleep(time.Duration(a) * time.Second)
+	result := a * 2
+	return result
+}
+
+func main() {
+	timed := MakeTimedFunction(timeMe).(func())
+	timed()
+	timedToo := MakeTimedFunction(timeMeToo).(func(int) int)
+	fmt.Println("timedToo(2):", timedToo(2))
+} //listend2
